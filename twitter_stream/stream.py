44652bd0926494b9ae9ac043bf086ead12265817
Jack Nicol <jack.nicol95@gmail.com>
2016-11-19 01:01:27 +0000

    Reinitialize

---
+++ twitter_stream/stream.py
@@ -0,0 +1,27 @@
+try:
+         import json
+except ImportError:
+        import simplejson as json
+
+from twitter import Twitter, OAuth, TwitterHTTPError,TwitterStream
+
+    # Variables that contains the user credentials to access Twitter API 
+ACCESS_TOKEN = '235228993-UMgntnuS8UKyGU7pitxvMNxQO4Eqte2tgAGk9ijK'
+ACCESS_SECRET = '9I8YOVtb6zIZaPpQEnAdVOZaq6vNBZJZVaRiU2OZir8os'
+CONSUMER_KEY = 'pPa5GLuxOLzK57woQ1pdYQIAf'
+CONSUMER_SECRET = 'IAsBqLL6lOQdcZ4VRu1ZIPvTOMIDw2Pa4bMbPtXbxP8Xwkjjd6'
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+
+
+twitter = Twitter(auth=oauth)
+recent_posts = twitter.search.tweets(q='from:@hm_morgan #GoogleAlerts ', result_type='recent', lang='en', count=2)
+
+text_file = open("twitter_stream_1000tweets.json", "w")
+
+text_file.write(json.dumps(recent_posts))
+
+text_file.close()
+
+print json.dumps(recent_posts)
---
+++ twitter_stream/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
+import datetime
+import twitter_stream
+import stream
+import json
+
+
+
+# Create your views here.
+from . import models as m
+
+def index(request):
+    try:
+        import json
+    except ImportError:
+        import simplejson as            json
+        
+    
+    with open('twitter_stream_1000tweets.json') as json_file:  
+        tweet = json.load(json_file)
+        tweet_text = []
+        tweet_profile_picture = []
+        tweet_expand_url = []
+        tweet_image = []
+        print len(tweet['statuses'])
+        for n in range(len(tweet['statuses'])):
+            tweet_text.append(tweet['statuses'][n]['text'])
+            tweet_profile_picture.append(tweet['statuses'][n]['user']['profile_image_url_https'])
+         
+            try:
+                tweet_expand_url.append(tweet['statuses'][n]['entities']['media'][0]['expanded_url'])
+                tweet_image.append(tweet['statuses'][n]['entities']['media'][0]['media_url'])
+            except KeyError:
+                tweet_image.append("")
+                tweet_expand_url.append("")
+            
+    template = loader.get_template('index.html')
+    context = Context({"tweet": tweet_text,  "tweet_profile":tweet_profile_picture,"twitterretweet":tweet_expand_url,"tweet_image": tweet_image, "range": range(len(tweet_text))})
+    
+    return HttpResponse(template.render(context))
