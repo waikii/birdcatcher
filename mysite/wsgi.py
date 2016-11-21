b2e589293e0ad1f54def257b0a43d88806107543
Jack Nicol <jack.nicol95@gmail.com>
2016-11-21 16:49:34 +0000

    heroku push

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -14,11 +14,6 @@ from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-application = get_wsgi_application()
-
-if not settings.DEBUG:
-    try:
-        from dj_static import Cling
-        application = Cling(get_wsgi_application())
-    except:
-        pass
+#application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
