c7cc0ef5db3f9396476e27d6abeeaaa90116ae71
Jack Nicol <jack.nicol95@gmail.com>
2016-11-21 16:35:07 +0000

    heroku push

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -16,3 +16,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
 
+if not settings.DEBUG:
+    try:
+        from dj_static import Cling
+        application = Cling(get_wsgi_application())
+    except:
+        pass
