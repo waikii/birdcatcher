8860d58fd62ddb5e60080662e5585681e3257e34
waikii <wai.kai.wu.14@aberdeen.ac.uk>
2017-02-03 21:27:56 +0000

    Fixed stylesheet hopefully

--- mysite/settings.py
+++ mysite/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'static_files')
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
